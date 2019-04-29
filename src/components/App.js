801a4a3b3c35ec6a40655f85494ef0efc1ef4bcd
pjliddy <pjliddy@gmail.com>
2019-04-28 20:58:38 -0400

    Added SearchBar component with placeholder event

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
-        Video App
+        <SearchBar />
       </div>
     );
   }
